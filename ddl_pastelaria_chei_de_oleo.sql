a19ff8087c290fe90756b9ec7946a05715dca7b5
José Henrique <135862618+josehlopes@users.noreply.github.com>
2023-11-15 01:16:44 -0300

    Update DDL

---
+++ ddl_pastelaria_chei_de_oleo.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE IF NOT EXISTS Chei_de_oleo_pastelaria;
+
+USE Chei_de_oleo_pastelaria;
+
+CREATE TABLE IF NOT EXISTS clientes (
+
+    idCliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomeCliente VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS enderecos (
+    idEndereco INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    logradouro VARCHAR(100) NOT NULL,
+    numero CHAR(10) NOT NULL,
+    cep VARCHAR(100) NOT NULL,
+    complemento VARCHAR(100),
+    idCliente INT NOT NULL,
+    FOREIGN KEY (idCliente) REFERENCES clientes (idCliente)
+);
+
+CREATE TABLE IF NOT EXISTS contatos (
+    idContato INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    telefone1 VARCHAR(100) NOT NULL,
+    telefone2 VARCHAR(100),
+    idCliente INT NOT NULL,
+    FOREIGN KEY (idCliente) REFERENCES clientes (idCliente)
+);
+
+CREATE TABLE IF NOT EXISTS produtos (
+    idProduto INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100),
+    preco DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pasteis (
+    idPastel INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(100),
+    preco DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pedidos (
+    idPedido INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    idCliente INT NOT NULL,
+    dataPedido DATE NOT NULL,
+    FOREIGN KEY (idCliente) REFERENCES clientes (idCliente)
+);
+
+CREATE TABLE IF NOT EXISTS detalhes_pedido (
+    idDetalhe INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    idPedido INT NOT NULL,
+    idProduto INT NOT NULL,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (idPedido) REFERENCES pedidos (idPedido),
+    FOREIGN KEY (idProduto) REFERENCES produtos (idProduto)
+);
