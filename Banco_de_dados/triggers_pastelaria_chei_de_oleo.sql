87b68764e3fcc8c9fbbb6b521dac804cabee8a04
José Henrique <135862618+josehlopes@users.noreply.github.com>
2023-11-26 18:55:05 -0300

    FEAT,FIX
    Criação da procedure:
    P_cadastrar_pastel
    Criação do trigger:
    TR_insere_ingredientes.
    
    e algumas pequenas modificações para o código rodar
    
    Co-Authored-By: Elisson Marques <nadsonnodachi@gmail.com>
    Co-Authored-By: Adriel Henrique <115196074+YGerard324@users.noreply.github.com>

--- Banco_de_dados/triggers_pastelaria_chei_de_oleo.sql
+++ Banco_de_dados/triggers_pastelaria_chei_de_oleo.sql
@@ -2,20 +2,19 @@
 
 DELIMITER $$
 
-CREATE TRIGGER TR_insere_ingrediente
-BEFORE INSERT ON pasteis
+CREATE OR REPLACE TRIGGER TR_insere_ingredientes
+AFTER INSERT ON pasteis
 FOR EACH ROW
-BEGIN
-    DECLARE idIngredienteExistente INT;
-
-    SELECT idIngrediente INTO idIngredienteExistente
-    FROM ingredientes
-    WHERE nome = NEW.descricao;
-
-    IF idIngredienteExistente IS NULL THEN
-        INSERT INTO ingredientes (nome, idCategoria)
-        VALUES (NEW.descricao, NEW.idCategoria);
-    END IF;
-END $$
+BEGIN    
+    DECLARE idPastelNovo INT;
+    SELECT idPastel INTO idPastelNovo FROM pasteis WHERE descricao = NEW.descricao AND tamanho = NEW.tamanho AND preco = NEW.preco;
+	#TESTE COM PASTEL DE TOMATE 4
+    INSERT INTO ingredientes_do_pastel (idPastel, idIngrediente)
+    VALUES (idPastelNovo, 3); -- Substitua 1 pelo ID do ingrediente desejado
+	#VALUES: REPETIR ID DO INGREDIENTE PARA idPastelNovo
+END$$
 
 DELIMITER ;
+
+
+SELECT * FROM V_ingredientes_do_pastel;
